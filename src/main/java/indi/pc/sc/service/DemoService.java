d56c4ebc6b5a63c3a140dd61da381461cc5d35ae
度旻 <dumin.psy@alibaba-inc.com>
2018-06-11 16:47:15 +0800

    2018-06-11 16:46

---
+++ src/main/java/indi/pc/sc/service/DemoService.java
@@ -0,0 +1,5 @@
+package indi.pc.sc.service;
+
+public interface DemoService {
+    String sayHello(String name);
+}
---
+++ src/main/java/indi/pc/sc/service/impl/DemoServiceImpl.java
@@ -0,0 +1,14 @@
+//package indi.pc.sc.service.impl;
+////
+////
+////
+////import indi.pc.sc.service.DemoService;
+////
+////@Service(timeout = 5000)
+////public class DemoServiceImpl implements DemoService {
+////
+////    @Override
+////    public String sayHello(String name) {
+////        return "Hello " + name;
+////    }
+////}
