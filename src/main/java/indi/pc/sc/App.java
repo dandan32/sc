49d8d3f8551f10d16a42c490fa07a0b9407080a9
度旻 <dumin.psy@alibaba-inc.com>
2018-06-11 16:46:47 +0800

    2018-06-11 16:46

--- src/main/java/indi/pc/sc/App.java
+++ src/main/java/indi/pc/sc/App.java
@@ -1,11 +1,11 @@
 package indi.pc.sc;
 
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.core.annotation.Order;
